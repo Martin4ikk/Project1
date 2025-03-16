4bc998426a0ce872c2f661f9dcb926a7f39356c2
Martin4ikk <shkolin.bohdan@gmail.com>
2025-03-16 17:11:15 +0200

    Add files via upload

---
+++ Unit1.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+ int secretNumber;
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+    srand(time(0));
+	secretNumber = rand() % 100 + 1;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+     int guess = StrToInt(Edit1->Text);
+	if (guess > secretNumber) {
+		Label1->Caption = "Занадто багато!";
+    } else if (guess < secretNumber) {
+		Label1->Caption = "Занадто мало!";
+    } else {
+		Label1->Caption = "Вітаю! Ви вгадали число!";
+	}
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+    secretNumber = rand() % 100 + 1;
+    Label1->Caption = "Нове число загадане!";
+	Edit1->Text = "";
+}
+//---------------------------------------------------------------------------
---
+++ Unit1.dfm
@@ -0,0 +1,56 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clHighlight
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 171
+    Top = 53
+    Width = 118
+    Height = 23
+    Color = clAqua
+    ParentColor = False
+  end
+  object Label2: TLabel
+    Left = 32
+    Top = 16
+    Width = 113
+    Height = 16
+    Caption = #1042#1075#1072#1076#1072#1081' '#1095#1080#1089#1083#1086
+    Color = clInactiveBorder
+    ParentColor = False
+  end
+  object Button1: TButton
+    Left = 32
+    Top = 96
+    Width = 121
+    Height = 25
+    Caption = 'ButtonCheck'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 32
+    Top = 53
+    Width = 121
+    Height = 23
+    TabOrder = 1
+  end
+  object Button2: TButton
+    Left = 32
+    Top = 127
+    Width = 121
+    Height = 25
+    Caption = 'ButtonNewGame'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+end
